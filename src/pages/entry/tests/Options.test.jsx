5ddc2194ca6a1041ff6158c5d47fb422697f5590
Apoka5555 <apoka5555@gmail.com>
2022-12-16 23:35:40 +0600

    Toppings

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -12,3 +12,18 @@ test("displays image from each scoop option from server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image from each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // finds images
+  const toppingImages = await screen.findAllByRole("img", { name: /topping/i });
+  expect(toppingImages).toHaveLength(3);
+
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
